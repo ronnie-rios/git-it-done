6a8b7d2105a264617358374481bd8ef22252b691
Ronnie Rios <ronniesmail14@gmail.com>
2021-04-20 23:33:41 -0500

    added ability to take user to github if more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +9,9 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data){
                 displayIssues(data);
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -45,6 +50,17 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
+}
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+   
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more isses on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank")
+    limitWarningEl.appendChild(linkEl);
 }
 
 getRepoIssues('facebook/react');
